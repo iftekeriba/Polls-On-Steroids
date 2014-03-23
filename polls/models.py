481d05dfe73d8a2d4c3eff15d6aead4a56b1395f
JBC <jbcsound@gmail.com>
2014-03-23 15:58:06 -0300

    Added Poll.was_published_recently

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,18 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Poll(models.Model):
+    def __str__(self):
+        return self.question
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    question = models.CharField(max_length=100)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    def __str__(self):
+        return self.choice_text
+    poll = models.ForeignKey(Poll)
+    choice_text = models.CharField(max_length=100)
+    votes = models.IntegerField(default=0)
