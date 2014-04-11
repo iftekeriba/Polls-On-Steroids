b845e932b716dadea3012bcd455637e60278c92b
JBC <jbcsound@gmail.com>
2014-04-11 04:22:56 -0300

    Changed to generic class based views and updated urlconf accordingly. Finished 4th part of the tutorial.

--- polls/views.py
+++ polls/views.py
@@ -1,24 +1,28 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from django.template import RequestContext, loader
+from django.views import generic
 from polls.models import Poll, Choice
 
-def index(request):
-    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    context = {'latest_poll_list': latest_poll_list,}
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_poll_list'
 
-def detail(request, poll_id):
-    poll = get_object_or_404(Poll, pk=poll_id)
-    return render(request, 'polls/detail.html', {'poll': poll})
+    def get_queryset(self):
+        return Poll.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/detail.html'
+
 
 def vote(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
     try:
         selected_choice = p.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        return render(request, 'polls/details.html',
+        return render(request, 'polls/detail.html',
             {'poll': p, 'error_message': "You didn't select a choice",}
         )
     else:
@@ -26,6 +30,7 @@ def vote(request, poll_id):
         selected_choice.save()
     return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
 
-def results(request, poll_id):
-    poll = get_object_or_404(Poll, pk=poll_id)
-    return render(request, 'polls/results.html', {'poll': poll})
+
+class ResultsView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/results.html'
