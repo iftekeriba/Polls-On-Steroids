4d0211210195f84d8bff2e2db6a96441103fc27c
JBC <jbcsound@gmail.com>
2014-04-11 05:32:01 -0300

    Fixed was_published_recently() bug (returned true for future polls) and added a test to check it.

--- polls/models.py
+++ polls/models.py
@@ -6,7 +6,8 @@ class Poll(models.Model):
     def __str__(self):
         return self.question
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date < now
     question = models.CharField(max_length=100)
     pub_date = models.DateTimeField('date published')
     was_published_recently.admin_order_field = 'pub_date'
