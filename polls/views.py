76f9018ac0b79b4b65b2d14beddee6c028b044bd
JBC <jbcsound@gmail.com>
2014-04-11 02:56:28 -0300

    Updated every view and template. Started 4th part.

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.template import RequestContext, loader
+from polls.models import Poll, Choice
 
 def index(request):
-    return HttpResponse("You're at the poll index")
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list,}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, poll_id):
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/detail.html', {'poll': poll})
+
+def vote(request, poll_id):
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/details.html',
+            {'poll': p, 'error_message': "You didn't select a choice",}
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+
+def results(request, poll_id):
+    get_object_or_404(Poll, pk=poll_id)
+    return render(request, )
